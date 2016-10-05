1ada8d1fe9bf58d2fab66e8142733255aac1e909
Chris <sosaca54@yahoo.com>
2016-10-04 19:18:02 -0500

    Player controls fixed to work with animations and have far less if/else statements.  Must go through and re-work some of the code still to clean it up, but will suffice for play testing

--- Blood Myth/Assets/Scripts/Movement.cs
+++ Blood Myth/Assets/Scripts/Movement.cs
@@ -1,17 +1,20 @@
-﻿ using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
-    
+
     public float normSpeed = 25;//Movement speed
     public float maxMovement = 10;//Movement speed cap
 
     public float sprintSpeed = 40;//Movement speed Sprinting
     public float sprintMaxMovement = 15; //Movement speed cap Sprinting
+    // Jump velocity
+    public float jumpVelocity = 20;
 
     float curSpeed = 0; //Current Move speed
-    float curMoverate = 0;//Current move rate
+    float curMoveRate = 0;//Current move rate
 
     /*
     public float changeTempMove = 0.0001f;
@@ -26,68 +29,106 @@ public class Movement : MonoBehaviour {
     public float changeTempJump = 0.01f;
     public float changeHydroJump = 0.01f;
    //*/
-    
+
 
     bool isGrounded = false; //Is player on the ground
     bool canClimb = false; //Can the player climb
-	
+
     string curMouseZone = "";//Store mouse zone
 
     private Animator _animController;
     private bool _facingRight = true;
+    private bool _sprinting = false;
+    private Rigidbody2D _rb2D;
+    private SpriteRenderer _flip;
 
-	void Start () 
+    public enum MoveStates
+    {
+        Idle = 0,
+        Walking = 1,
+        Sprinting = 2,
+        Jumping = 3,
+    };
+    private MoveStates moveState = MoveStates.Idle;
+
+    void Start()
     {
         curSpeed = normSpeed;
-        curMoverate = maxMovement;
+        curMoveRate = maxMovement;
         this._animController = this.gameObject.GetComponent<Animator>();
-	}
-	
-	
-	void Update () 
+        this._animController.SetInteger("moveState", (int)moveState);
+        this._rb2D = GetComponent<Rigidbody2D>();
+        this._flip = GetComponent<SpriteRenderer>();
+    }
+
+
+    void FixedUpdate()
     {
         //Keyboard Movement
         //*
         //Sprinting
         if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
         {
+            this._sprinting = true;
             curSpeed = sprintSpeed;
-            curMoverate = sprintMaxMovement;
+            curMoveRate = sprintMaxMovement;
             Player.Hydration -= 0.001f;
             Player.Tempurature += 0.001f;
-            this._animController.SetBool("Sprinting", true);
-        }
-        else if (!isGrounded)//Slower in air
-        {
-            curSpeed = normSpeed / 2;
+            moveState = MoveStates.Sprinting;
+            this._animController.SetInteger("moveState", (int)moveState);
         }
         else
         {
             curSpeed = normSpeed;
-            curMoverate = maxMovement;
-            this._animController.SetBool("Sprinting", false);
+            curMoveRate = maxMovement;
         }
         //*
         //Moving
-        if (Input.GetKey(KeyCode.A) && GetComponent<Rigidbody2D>().velocity.x > -curMoverate)//Moving Left
-        {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(-curSpeed, 0));
-            this._animController.SetBool("Moving", true);
+        //if (Input.GetKey(KeyCode.A)/* && GetComponent<Rigidbody2D>().velocity.x > -curMoveRate*/)//Moving Left
+        //{
+        //    GetComponent<Rigidbody2D>().AddForce(-transform.right * curSpeed);
+        //    GetComponent<SpriteRenderer>().flipX = true;
+        //    moveState = MoveStates.Walking;
+        //    this._animController.SetInteger("moveState", (int)moveState);
 
-        }
-        else if (Input.GetKey(KeyCode.D) && GetComponent<Rigidbody2D>().velocity.x < curMoverate)//Moving Right
+        //}
+        //else if (Input.GetKey(KeyCode.D)/* && GetComponent<Rigidbody2D>().velocity.x < curMoveRate*/)//Moving Right
+        //{
+        //    GetComponent<Rigidbody2D>().AddForce(transform.right * curSpeed);
+        //    GetComponent<SpriteRenderer>().flipX = false;
+        //    moveState = MoveStates.Walking;
+        //    this._animController.SetInteger("moveState", (int)moveState);
+        //}
+
+        float move = Input.GetAxis("Horizontal");
+        // Sprite orientation
+        if (move < 0 && this._facingRight)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(curSpeed, 0));
-            this._animController.SetBool("Moving", true);
+            this._flip.flipX = true;
+            this._facingRight = false;
         }
+        else if (move > 0 && !this._facingRight)
+        {
+            this._flip.flipX = false;
+            this._facingRight = true;
+        }
+        // Add velocity up to max move rate
+        this._rb2D.velocity = new Vector2(move * this.curMoveRate, this._rb2D.velocity.y);
+        // Need to fiddle with these values for the movement shit (probably want to change the state machine to better reflect what we want (i.e. sprint bool and move bool)
+        if (Mathf.Abs(this._rb2D.velocity.x) > 0 && !this._sprinting)
+        {
+            moveState = MoveStates.Walking;
+        }
+        // Update animations
+        this._animController.SetInteger("moveState", (int)moveState);
 
         //Climbing
         if (Input.GetKey(KeyCode.W) && canClimb)//Climbing Up
         {
             GetComponent<Rigidbody2D>().gravityScale = 0;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             GetComponent<BoxCollider2D>().isTrigger = true;
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, curSpeed * 5));
+            GetComponent<Rigidbody2D>().AddForce(transform.up * curSpeed);
             Player.Hydration -= 0.01f;
             Player.Tempurature += 0.01f;
 
@@ -95,9 +136,9 @@ public class Movement : MonoBehaviour {
         else if (Input.GetKey(KeyCode.S) && canClimb)//Climbing Down
         {
             GetComponent<Rigidbody2D>().gravityScale = 0;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             GetComponent<BoxCollider2D>().isTrigger = true;
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -curSpeed * 5));
+            GetComponent<Rigidbody2D>().AddForce(-transform.up * curSpeed);
             Player.Hydration -= 0.01f;
             Player.Tempurature += 0.01f;
         }
@@ -108,14 +149,17 @@ public class Movement : MonoBehaviour {
         }
 
         //Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded || Input.GetKeyDown(KeyCode.S) && isGrounded)//Jumping
-        {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, curSpeed * 15));
-            Player.Hydration -= 0.1f;
-            Player.Tempurature += 0.1f;
-            this._animController.SetBool("Jumping", true);
-        }
+        //if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.S)))//Jumping
+        //{
+        //    GetComponent<Rigidbody2D>().AddForce(transform.up * curSpeed);
+        //    Player.Hydration -= 0.1f;
+        //    Player.Tempurature += 0.1f;
+        //    moveState = MoveStates.Jumping;
+        //    this._animController.SetInteger("moveState", (int)moveState);
+        //}
         //*/
+
+        
 
         //Mouse Movement
         //*
@@ -124,15 +168,19 @@ public class Movement : MonoBehaviour {
         if (Input.GetMouseButton(0))
         {
             CheckZone();
-            if (curMouseZone == "Left" && GetComponent<Rigidbody2D>().velocity.x > -curMoverate)//Movement
+            if (curMouseZone == "Left"/* && GetComponent<Rigidbody2D>().velocity.x > -curMoveRate*/)//Movement
             {
                 GetComponent<Rigidbody2D>().AddForce(new Vector2(-curSpeed, 0));
-                this._animController.SetBool("Moving", true);
+                GetComponent<SpriteRenderer>().flipX = true;
+                moveState = MoveStates.Walking;
+                this._animController.SetInteger("moveState", (int)moveState);
             }
-            else if (curMouseZone == "Right" && GetComponent<Rigidbody2D>().velocity.x < curMoverate)//Movement
+            else if (curMouseZone == "Right" /*&& GetComponent<Rigidbody2D>().velocity.x < curMoveRate*/)//Movement
             {
                 GetComponent<Rigidbody2D>().AddForce(new Vector2(curSpeed, 0));
-                this._animController.SetBool("Moving", true);
+                GetComponent<SpriteRenderer>().flipX = false;
+                moveState = MoveStates.Walking;
+                this._animController.SetInteger("moveState", (int)moveState);
             }
 
         }
@@ -142,16 +190,16 @@ public class Movement : MonoBehaviour {
             if (curMouseZone == "Top")//Action Button
             {
                 //Always Climbing
-//                if (canClimb)//Climbing Up
-//                {
-//                    GetComponent<Rigidbody2D>().gravityScale = 0;
-//                    GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
-//                    GetComponent<BoxCollider2D>().isTrigger = true;
-//                    GetComponent<Rigidbody2D>().AddForce(new Vector2(0, curSpeed * 5));
-//                    Player.Hydration -= 0.01f;
-//                    Player.Tempurature += 0.01f;
-//
-//                }
+                //                if (canClimb)//Climbing Up
+                //                {
+                //                    GetComponent<Rigidbody2D>().gravityScale = 0;
+                //                    GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+                //                    GetComponent<BoxCollider2D>().isTrigger = true;
+                //                    GetComponent<Rigidbody2D>().AddForce(new Vector2(0, curSpeed * 5));
+                //                    Player.Hydration -= 0.01f;
+                //                    Player.Tempurature += 0.01f;
+                //
+                //                }
 
 
             }
@@ -160,32 +208,41 @@ public class Movement : MonoBehaviour {
                 GetComponent<Rigidbody2D>().AddForce(new Vector2(0, curSpeed * 15));
                 Player.Hydration -= 0.1f;
                 Player.Tempurature += 0.01f;
-                this._animController.SetTrigger("Jumping");
+                this.moveState = MoveStates.Jumping;
+                this._animController.SetInteger("moveState", (int)this.moveState);
             }
 
         }
 
         //this._animController.SetBool("Moving", false);
         //*/
-        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.magnitude) < 1.0f)
+        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.magnitude) < 1.0f && isGrounded)
         {
-            this._animController.SetBool("Moving", false);
-        }
-
-        if (GetComponent<Rigidbody2D>().velocity.x > 0 && !this._facingRight)
-        {
-            this.Flip();
-        }
-        else if (GetComponent<Rigidbody2D>().velocity.x < 0 && this._facingRight)
-        {
-            this.Flip();
+            this.moveState = MoveStates.Idle;
+            this._animController.SetInteger("moveState", (int)this.moveState);
         }
     }
 
+    void Update()
+    {
+        if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.S)))
+        {
+            this.moveState = MoveStates.Jumping;
+            this._animController.SetInteger("moveState", (int)this.moveState);
+            this._rb2D.AddForce(new Vector2(0, jumpVelocity));
+        }
+
+        if (!isGrounded)
+        {
+            curMoveRate = normSpeed / 2;
+            this.moveState = MoveStates.Jumping;
+            this._animController.SetInteger("moveState", (int)this.moveState);
+        }
+    }
 
     void CheckZone()
     {
-        if (Input.mousePosition.y > ((float)Screen.height / (float)Screen.width) * Input.mousePosition.x) 
+        if (Input.mousePosition.y > ((float)Screen.height / (float)Screen.width) * Input.mousePosition.x)
         {
 
             if (Input.mousePosition.y > (-(float)Screen.height / (float)Screen.width) * Input.mousePosition.x + Screen.height) // Top Button
@@ -222,8 +279,8 @@ public class Movement : MonoBehaviour {
 
     void OnTriggerStay2D(Collider2D col)
     {
-        
-        if(col.gameObject.tag == "Climb")
+
+        if (col.gameObject.tag == "Climb")
             canClimb = true;
 
         if (col.gameObject.tag == "Water" && Input.GetKeyDown(KeyCode.W))
@@ -232,10 +289,10 @@ public class Movement : MonoBehaviour {
 
         if (col.gameObject.tag == "Shade")
         {
-            if( Player.Tempurature > 95)
-                Player.Tempurature -= 0.01f;  
+            if (Player.Tempurature > 95)
+                Player.Tempurature -= 0.01f;
 
-            else if( Player.Tempurature < 95)
+            else if (Player.Tempurature < 95)
                 Player.Tempurature += 0.01f;
         }
 
@@ -253,7 +310,7 @@ public class Movement : MonoBehaviour {
 
     void OnTriggerExit2D(Collider2D col)
     {
-        if(col.gameObject.tag == "Climb")
+        if (col.gameObject.tag == "Climb")
             canClimb = false;
 
     }
@@ -272,13 +329,5 @@ public class Movement : MonoBehaviour {
         {
             isGrounded = false;
         }
-    }
-
-    void Flip()
-    {
-        this._facingRight = !this._facingRight;
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
     }
 }
