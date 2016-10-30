008e108d60ab8e600f743ce1b1938e70259a3277
jimbo23 <jniemie1@mail.depaul.edu>
2016-10-30 02:03:58 -0500

    Added combined Test Area. Minor Issue with buttons not working, but will be fixed soon.

---
+++ Blood Myth/Assets/Scripts/Jim's Test Scene Script(s)/TestArenaChange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestArenaChange : MonoBehaviour {
+
+    public GameObject mainCharacter;
+
+
+    public void ChangeArena(Transform spawnPoint)
+    {
+        Debug.Log("boop");
+        mainCharacter.transform.position = spawnPoint.position;
+    }
+
+}
