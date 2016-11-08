8c2e70bf47721c70b34a5605d118a12a35d20e2a
Chris <sosaca54@yahoo.com>
2016-11-08 15:00:19 -0600

    Update to player scripts to account for a 'better' fatigue system.  Temperature hazard script added to replace cold/hot hazard scripts because now we only care about negative effects to temperature and not hot/cold effects separately.

---
+++ Blood Myth/Assets/Scripts/Environment/TemperatureHazard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TemperatureHazard : MonoBehaviour
+{
+    public GameObject player;
+    public TemperatureEffect hazardEffect;
+
+    void OnTriggerStay2D(Collider2D inPlayer)
+    {
+        if (inPlayer.gameObject.tag == "Player")
+        {
+            // Flag needed to change texture material for arms
+            player.GetComponent<Player>().TemperatureHazard(hazardEffect);
+        }
+    }
+}
