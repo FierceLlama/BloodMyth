778ceab7a6c67a4bffffcc5f5628ee0c832ae007
Omar Zohdi <omarzohdi@gmail.com>
2017-01-12 05:03:44 +0100

    First Pass at Dialogue
    Created a parser and a basic id system using strings for simplifing the
    process of adding text-files to the dialogue. I'm using Unity's Text
    Assets to store the dialogue and extracting the lines to be used in the
    dialogue.

---
+++ Blood Myth/Assets/Scripts/Systems/Dialogue Management/DialogueLibrary.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DialogueHandle
+{
+    public string dialogueKey;
+    public string directory;
+
+    public DialogueHandle()
+    {
+        directory = "dialogue";
+    }
+}
+public class DialogueData
+{
+    public DialogueHandle handle;
+    public List<string> Lines = new List<string>();
+
+    public void AddLines (string[] inLines)
+    {
+        Lines = new List<string>(inLines);
+    }
+    public void AddLine (string inLine)
+    {
+        Lines.Add(inLine);
+    }
+}
+public class DialogueLibrary
+{
+    List<DialogueHandle> CurrentActiveDialogues;
+    
+    public DialogueLibrary()
+    {
+        CurrentActiveDialogues = new List<DialogueHandle>();
+    }
+
+    public DialogueData FetchDialogueData(string inKey)
+    {
+        DialogueData Data = new DialogueData();
+
+        DialogueHandle Handle = this.FindDialogueHandle(inKey);
+        this.ParseDialogueFile(Data, Handle);
+
+        return Data;
+    }
+    public void PreLoadDialogueHandle(string inKey)
+    {
+        CurrentActiveDialogues.Add(ParseDialogueKey(inKey));
+    }
+
+    private DialogueHandle ParseDialogueKey(string inKey)
+    {
+        //Temporary Format// 111222333
+        //FALL TOTEM 1    // FLLTTM001
+        DialogueHandle handle = new DialogueHandle();
+        handle.dialogueKey = inKey;
+
+        handle.directory += "/" + TranslateKey(inKey.Substring(0, 3));
+        handle.directory += "/" + TranslateKey(inKey.Substring(3, 3));
+        handle.directory += "/" + inKey.Substring(6, 3);
+       
+        return handle;
+    }
+    private string TranslateKey(string ParseString)
+    {
+        switch (ParseString)
+        {
+            case "FLL": return "fall";
+            case "TTM": return "totem";
+            default: return "";
+        }
+    }
+
+    private void ParseDialogueFile(DialogueData inData, DialogueHandle inHandle)
+    {
+
+        ///Use Unity's Text Assets.
+        inData.handle = inHandle;
+        TextAsset x = Resources.Load(inHandle.directory) as TextAsset;
+ 
+        using (StringReader reader = new StringReader(x.text))
+        {
+            string line = string.Empty;
+            do
+            {
+                line = reader.ReadLine();
+                if (line != null)
+                {
+                    inData.AddLine(line);           
+                }
+
+            } while (line != null);
+        }
+
+        //File.ReadAllLines("Resources/"+inHandle.directory);
+        
+        //inData.AddLines (File.ReadAllLines(inHandle.directory));
+    }
+    private DialogueHandle FindDialogueHandle(string Key)
+    {
+        return CurrentActiveDialogues.Find(x => (x.dialogueKey == Key));
+    }
+}
---
+++ Blood Myth/Assets/Scripts/Systems/Dialogue Management/DialogueManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+    DialogueLibrary dialogueLibrary;
+    DialogueData CurrentDialogue;
+
+    void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+            InitDialogueManager();
+        }
+        else if (Instance != this)
+            DestroyImmediate(this);
+    }
+
+    void InitDialogueManager()
+    {
+        dialogueLibrary = new DialogueLibrary();
+    }
+
+    /// TEST
+    public void LoadDialogueKeys()
+    {
+        dialogueLibrary.PreLoadDialogueHandle("FLLTTM001");
+    } 
+
+    public bool StartDialogue(string DialogueKey)
+    {
+        CurrentDialogue = dialogueLibrary.FetchDialogueData(DialogueKey);
+
+        if (CurrentDialogue != null)
+        {
+            //TODO(OMAR)
+            //Start Dialogue Interface// etc..
+            //Change the Games State?
+            //Make sure the Pause Function works in HERE
+            return true;
+        }
+   
+        return false;
+    }
+}
