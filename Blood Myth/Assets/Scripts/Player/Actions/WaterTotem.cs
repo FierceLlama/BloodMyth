d9fd28a727397a8da42453103496e09c6a869a36
Chris <sosaca54@yahoo.com>
2017-02-28 13:29:38 -0600

    Icons properly displaying for climbing and drinking water

--- Blood Myth/Assets/Scripts/Player/Actions/WaterTotem.cs
+++ Blood Myth/Assets/Scripts/Player/Actions/WaterTotem.cs
@@ -2,26 +2,35 @@
 using System.Collections;
 
 public class WaterTotem : MonoBehaviour
-{
+    {
     public GameObject _player;
 
-    void OnTriggerStay2D(Collider2D player)
-    {
-#if UNITY_EDITOR
-        if (player.gameObject.tag == "Player" && Input.GetKey(KeyCode.W))
+    void OnTriggerStay2D(Collider2D inPlayer)
         {
+        GameObject.FindWithTag("Actions").GetComponent<SetActionIcon>().DisplayIcon(SetActionIcon.IconType.DRINK_WATER);
+#if UNITY_EDITOR
+        if (inPlayer.gameObject.tag == "Player" && Input.GetKey(KeyCode.W))
+            {
             this._player.GetComponent<Player>().DrinkingWater();
-        }
+            }
 #endif
 #if UNITY_ANDROID
-        if (player.gameObject.tag == "Player" &&
+        if (inPlayer.gameObject.tag == "Player" &&
             (this._player.GetComponent<Player>().getPrimaryTouch().CurrentScreenSection == ScreenSection.Top &&
             this._player.GetComponent<Player>().getPrimaryTouch().getTouchPhase() == TouchPhase.Stationary)
             || (this._player.GetComponent<Player>().getSecondaryTouch().CurrentScreenSection == ScreenSection.Top &&
             this._player.GetComponent<Player>().getSecondaryTouch().getTouchPhase() == TouchPhase.Stationary))
             {
-            player.GetComponent<Player>().DrinkingWater();
+            inPlayer.GetComponent<Player>().DrinkingWater();
             }
 #endif
         }
-}
+
+    void OnTriggerExit2D(Collider2D inPlayer)
+        {
+        if (inPlayer.gameObject.tag == "Player")
+            {
+            GameObject.FindWithTag("Actions").GetComponent<SetActionIcon>().HideIcon();
+            }
+        }
+    }
---
+++ Blood Myth/Assets/SetActionIcon.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActionIcon : MonoBehaviour
+    {
+    public enum IconType
+        {
+        CLIMB,
+        DRINK_WATER,
+        UNINITIALIZED
+        }
+
+    private IconType _type;
+    private UnityEngine.UI.Image _image;
+    public Sprite climbingIcon;
+    public Sprite drinkingIcon;
+
+    void Start()
+        {
+        this._type = IconType.UNINITIALIZED;
+        this._image = this.GetComponent<UnityEngine.UI.Image>();
+        this.HideIcon();
+        }
+
+    public void DisplayIcon(IconType inType)
+        {
+        switch (inType)
+            {
+            case IconType.CLIMB:
+                this._image.sprite = this.climbingIcon;
+                break;
+            case IconType.DRINK_WATER:
+                this._image.sprite = this.drinkingIcon;
+                break;
+            }
+        this._image.enabled = true;
+        }
+
+    public void HideIcon()
+        {
+        this._image.enabled = false;
+        }
+    }
