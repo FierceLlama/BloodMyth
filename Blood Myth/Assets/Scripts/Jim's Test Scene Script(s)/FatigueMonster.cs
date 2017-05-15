dec9810695f4a88b08de59d88ce34ac1e65587c1
jimn23 <jimncomputer23@aim.com>
2017-05-15 01:33:44 -0500

    Added new music and Fatigue Monster prefab in prefabs folder. moves left to right. You can climb now, but you can't stop climbing.

---
+++ Blood Myth/Assets/Scripts/Jim's Test Scene Script(s)/FatigueMonster.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatigueMonster : MonoBehaviour {
+
+    public float timeToTake;
+    private GameObject _xForm;
+    public GameObject left;
+    public GameObject right;
+    public float stoppingDistance = 0.000f;
+    private bool goingRight = false;
+    private bool goingLeft = false;
+    private bool waiting = false;
+
+    void Start()
+    {
+        goingRight = true;
+
+        this._xForm = this.gameObject;
+        this._xForm.transform.position = this.left.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (checkDistance(this.right.transform) && goingRight)
+        {
+            this.MoveTo();
+        }
+        else if (!checkDistance(this.right.transform) && goingRight)
+        {
+            goingRight = false;
+            goingLeft = true;
+            this._xForm.transform.position = this.right.transform.position;
+        }
+        if(checkDistance(this.left.transform) && goingLeft)
+        {
+            this.MoveFrom();
+        }
+        else if (!checkDistance(this.left.transform) && goingLeft)
+        {
+            goingLeft = false;
+            goingRight = true;
+            this._xForm.transform.position = this.left.transform.position;
+        }
+    }
+
+    bool checkDistance(Transform endPos)
+    {
+        bool distanceCheck = false;
+        if (Vector2.Distance(this._xForm.transform.position, endPos.position) > this.stoppingDistance)
+        {
+            distanceCheck = true;
+        }
+        return distanceCheck;
+    }
+
+    void MoveTo()
+    {
+        this._xForm.transform.position = Vector2.MoveTowards(this._xForm.transform.position, right.transform.position, Time.deltaTime * timeToTake);
+    }
+
+    void MoveFrom()
+    {
+        this._xForm.transform.position = Vector2.MoveTowards(this._xForm.transform.position, left.transform.position, Time.deltaTime * timeToTake);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            
+        }
+    }
+
+
+}
--- Blood Myth/Assets/Scripts/Player/NormalPlayer.cs
+++ Blood Myth/Assets/Scripts/Player/NormalPlayer.cs
@@ -23,8 +23,8 @@ public class NormalPlayer : FatigueStateBaseClass
 #if UNITY_EDITOR
         this.player.SetMove(Input.GetAxis("Horizontal"));
         this.player.GetRigidbody().velocity = new Vector2(this.player.GetMove() * 25, this.player.GetRigidbody().velocity.y);
-        if (Input.GetKey(KeyCode.LeftShift) && this.player.GetMove() != 0 && !this.player.GetSprinting())
-            {
+        if (Input.GetKey(KeyCode.LeftShift) && this.player.GetMove() != 0 && !this.player.GetSprinting()) 
+        {
             this.player.SetSprinting(true);
             if (!this.player.GetJumping())
                 {
@@ -32,7 +32,7 @@ public class NormalPlayer : FatigueStateBaseClass
                 }
             }
         else if (Input.GetKeyUp(KeyCode.LeftShift) && this.player.GetMoving())
-            {
+        {
             this.player.SetSprinting(false);
             if (!this.player.GetJumping())
                 {
@@ -41,7 +41,7 @@ public class NormalPlayer : FatigueStateBaseClass
             }
 
         if (Input.GetKeyDown(KeyCode.Space) && !this.player.GetJumping() && this.player.GetGrounded())
-            {
+        {
             this.player.SetJumping(true);
             this.player.SetIHaveChangedState(true);
             this.player.GetRigidbody().velocity = new Vector2(this.player.GetRigidbody().velocity.x, 20);
@@ -54,7 +54,7 @@ public class NormalPlayer : FatigueStateBaseClass
             this.player.StartedClimbingVertically();
             this.player.DeterminePlayerClimbDirection();
             }
-        else if(Input.GetKeyDown(KeyCode.W) && this.player.GetCanClimb() && this.player.isActivelyClimbing())
+        else if(Input.GetKey(KeyCode.W) && !this.player.GetCanClimb() && this.player.isActivelyClimbing()) // changed to GetKey and added the ! 
             {
             this.player.DeterminePlayerClimbDirection();
             }
@@ -80,12 +80,15 @@ public class NormalPlayer : FatigueStateBaseClass
 
         if (this.player.GetIHaveChangedState())
             {
-            if (this.player.GetJumping())
+            if (this.player.GetJumping() && !this.player.isActivelyClimbing())
                 {
                 this.player.skeletonAnimation.state.SetAnimation(0, "Jump", false);
                 }
-
-            else if (this.player.GetMoving() && !this.player.GetJumping())
+            else if (this.player.isActivelyClimbing())
+            {
+                this.player.skeletonAnimation.state.SetAnimation(0, "Climb", false); 
+            }
+            else if (this.player.GetMoving() && !this.player.GetJumping() && !this.player.isActivelyClimbing())
                 {
                 if (this.player.GetSprinting())
                     {
