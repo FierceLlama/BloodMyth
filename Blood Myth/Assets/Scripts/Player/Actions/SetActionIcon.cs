f1f72ded4ceb0210a7d1dafc7bc5185f448fac88
Chris <sosaca54@yahoo.com>
2017-06-02 10:51:28 -0500

    Added UI, need to attach event triggers to buttons to work with player scripts

--- Blood Myth/Assets/Scripts/Player/Actions/SetActionIcon.cs
+++ Blood Myth/Assets/Scripts/Player/Actions/SetActionIcon.cs
@@ -6,32 +6,37 @@ public class SetActionIcon : MonoBehaviour
     {
     public enum IconType
         {
-        CLIMB,
+        CAPE,
         DRINK_WATER,
+        INVESTIGATE,
         UNINITIALIZED
         }
 
     private IconType _type;
     private UnityEngine.UI.Image _image;
-    public Sprite climbingIcon;
+    public Sprite capeIcon;
     public Sprite drinkingIcon;
+    public Sprite investigateIcon;
 
     void Start()
         {
         this._type = IconType.UNINITIALIZED;
         this._image = this.GetComponent<UnityEngine.UI.Image>();
-        this.HideIcon();
+        this.DisplayIcon(IconType.CAPE);
         }
 
     public void DisplayIcon(IconType inType)
         {
         switch (inType)
             {
-            case IconType.CLIMB:
-                this._image.sprite = this.climbingIcon;
+            case IconType.CAPE:
+                this._image.sprite = this.capeIcon;
                 break;
             case IconType.DRINK_WATER:
                 this._image.sprite = this.drinkingIcon;
+                break;
+            case IconType.INVESTIGATE:
+                this._image.sprite = this.investigateIcon;
                 break;
             }
         this._image.enabled = true;
