916fa864d6e9611e8d5d14f68e36a1690765999b
Chris <sosaca54@yahoo.com>
2017-06-21 13:50:28 -0500

    Updated Fall to have moving platforms be more platformery.  Not sure if this code should be added to other areas where jumping may be an issue

---
+++ Blood Myth/Assets/Scripts/Gameplay/Platforming.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforming : MonoBehaviour
+    {
+    public GameObject trigger;
+
+    void OnTriggerEnter2D(Collider2D inPlayer)
+        {
+        if (inPlayer.gameObject.tag == "Player")
+            {
+            this.trigger.GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+
+    void OnTriggerExit2D(Collider2D inPlayer)
+        {
+        if (inPlayer.gameObject.tag == "Player")
+            {
+            this.trigger.GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+    }
