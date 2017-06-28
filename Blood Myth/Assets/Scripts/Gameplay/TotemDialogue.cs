c857b1d40e68f6b68a814ae9c71fd50603356b98
Chris <sosaca54@yahoo.com>
2017-06-28 15:49:15 -0500

    Added new summer and assets.  Dialogue fixed requiring player to interact for dialogue.

--- Blood Myth/Assets/Scripts/Gameplay/TotemDialogue.cs
+++ Blood Myth/Assets/Scripts/Gameplay/TotemDialogue.cs
@@ -5,13 +5,41 @@ using UnityEngine;
 public class TotemDialogue : MonoBehaviour
     {
     public string DialogueKey;
+    public GameObject _player;
+    private Player _playerScript;
+    private SetActionIcon _action;
+
+    void Start()
+        {
+        this._playerScript = this._player.GetComponent<Player>();
+        this._action = GameObject.FindWithTag("Actions").GetComponent<SetActionIcon>();
+        }
 
     void OnTriggerEnter2D(Collider2D inPlayer)
         {
         if (inPlayer.gameObject.tag == "Player")
             {
-            DialogueManager.Instance.LoadDialogueKeys(DialogueKey);
-            DialogueManager.Instance.StartDialogue(DialogueKey);
+            this._action.DisplayIcon(SetActionIcon.IconType.INVESTIGATE);
+            }
+        }
+
+    void OnTriggerStay2D(Collider2D inPlayer)
+        {
+        if (inPlayer.gameObject.tag == "Player")
+            {
+            if (InputManager.instance.GetInvestigateActive())
+                {
+                DialogueManager.Instance.LoadDialogueKeys(DialogueKey);
+                DialogueManager.Instance.StartDialogue(DialogueKey);
+                }
+            }
+        }
+
+    void OnTriggerExit2D(Collider2D inPlayer)
+        {
+        if (inPlayer.gameObject.tag == "Player")
+            {
+            this._action.HideIcon();
             }
         }
     }
