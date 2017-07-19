460ddfbdfe99b7bf033e77395895ea1fb78e2ac5
Chris <sosaca54@yahoo.com>
2017-07-19 16:32:58 -0500

    Yay for statics remembering events.

--- Blood Myth/Assets/Scripts/Environment/SpringBreakGround.cs
+++ Blood Myth/Assets/Scripts/Environment/SpringBreakGround.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class SpringBreakGround : MonoBehaviour
     {
+    static bool doneThisBefore = false;
+
+    void Start()
+        {
+        if (doneThisBefore)
+            {
+            this.gameObject.SetActive(false);
+            }
+        }
+
     void OnTriggerEnter2D(Collider2D inPlayer)
         {
-        if (inPlayer.gameObject.tag == "Player")
+        if (inPlayer.gameObject.tag == "Player" && !doneThisBefore)
             {
+            doneThisBefore = true;
             this.gameObject.SetActive(false);
             }
         }
