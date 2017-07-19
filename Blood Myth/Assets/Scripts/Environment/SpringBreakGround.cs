74d8d83c678e88a8baf8dff09ef794b38b93f866
Chris <sosaca54@yahoo.com>
2017-07-19 16:12:58 -0500

    Updated duplicate assets for removal.  Added break away ground for Spring level.  Updated all floating platforms to have the platformery script attached. Need to scrub through on non edge platforms to remove latformery script

---
+++ Blood Myth/Assets/Scripts/Environment/SpringBreakGround.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringBreakGround : MonoBehaviour
+    {
+    void OnTriggerEnter2D(Collider2D inPlayer)
+        {
+        if (inPlayer.gameObject.tag == "Player")
+            {
+            this.gameObject.SetActive(false);
+            }
+        }
+    }
